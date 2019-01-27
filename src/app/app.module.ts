e805b026827b5843cbd31d2568538056957a7255
Pavel Koroteev <Pavel.koroteev@comindware.com>
2019-01-27 16:20:03 +0300

    add Table.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { NgModule } from '@angular/core';
 import {
   MatToolbarModule,
   MatButtonModule,
-  MatIconModule
+  MatGridListModule,
+  MatTableModule
  } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { TableComponent } from './table/table.component';
+import { GanttComponent } from './gantt/gantt.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ContentComponent,
+    TableComponent,
+    GanttComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,7 +28,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatGridListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
