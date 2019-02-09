3c2f8d75b8d6937c55b8abb84ef5ad405982c937
Vladislav Smirnov <vs@comindware.com>
2019-02-09 20:06:47 +0300

    Drow some tasks

--- src/app/components/gantt/gantt.component.ts
+++ src/app/components/gantt/gantt.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import TableModel from '../../models/TableModel';
+import { AppState } from '../../app.state';
+
 @Component({
-  selector: "app-gantt",
-  templateUrl: "./gantt.component.html",
-  styleUrls: ["./gantt.component.css"]
+  selector: 'app-gantt',
+  templateUrl: './gantt.component.html',
+  styleUrls: ['./gantt.component.css']
 })
 export class GanttComponent implements OnInit {
-  constructor() {}
+  rows: Observable<TableModel[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.rows = store.select('tutorial');
+  }
 
   ngOnInit() {}
 }
