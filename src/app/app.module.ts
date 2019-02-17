36e7bb9ee3d43ff71f3b55e760c43da336a944fc
Vladislav Smirnov <vs@comindware.com>
2019-02-17 21:54:32 +0300

    Improve modules connection. Fixes to events.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,64 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatGridListModule,
-  MatFormFieldModule,
-  MatTableModule,
-  MatInputModule,
-  MatSelectModule
-} from "@angular/material";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ToolbarComponent } from "./components/toolbar/toolbar.component";
-import { ContentComponent } from "./content/content.component";
-import { TableComponent } from "./components/table/table.component";
-import { GanttComponent } from "./components/gantt/gantt.component";
-import { StoreModule } from "@ngrx/store";
-import { reducer } from "./reducers/TableReducer";
-import { TaskComponent } from "./components/task/task.component";
-import { SubtaskComponent } from "./components/subtask/subtask.component";
-import { SectionComponent } from "./components/section/section.component";
-import { MilestoneComponent } from "./components/milestone/milestone.component";
-import { TimelineComponent } from "./components/timeline/timeline.component";
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ToolbarComponent,
-    ContentComponent,
-    TableComponent,
-    GanttComponent,
-    TaskComponent,
-    SubtaskComponent,
-    SectionComponent,
-    MilestoneComponent,
-    TimelineComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatGridListModule,
-    MatTableModule,
     MatFormFieldModule,
+    MatTableModule,
     MatInputModule,
     MatSelectModule,
-    StoreModule.forRoot({
-      tutorial: reducer
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    MatIconModule
+} from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { TableComponent } from './components/table/table.component';
+import { GanttComponent } from './components/gantt/gantt.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/TableReducer';
+import { TaskComponent } from './components/task/task.component';
+import { SubtaskComponent } from './components/subtask/subtask.component';
+import { SectionComponent } from './components/section/section.component';
+import { MilestoneComponent } from './components/milestone/milestone.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        ToolbarComponent,
+        ContentComponent,
+        TableComponent,
+        GanttComponent,
+        TaskComponent,
+        SubtaskComponent,
+        SectionComponent,
+        MilestoneComponent,
+        TimelineComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatGridListModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule,
+        FormsModule,
+        StoreModule.forRoot({
+            tutorial: reducer
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -2,57 +2,66 @@ import { Component, OnInit, Input } from '@angular/core';
 import TableModel from '../../models/TableModel';
 
 interface DragContext {
-  startPointOfMove?: number;
-  type?: 'start' | 'end';
-  startDate?: number;
-  duration?: number;
+    startPointOfMove?: number;
+    type?: 'start' | 'end';
+    startDate?: number;
+    duration?: number;
 }
 @Component({
-  selector: '[app-task]',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+    selector: '[app-task]',
+    templateUrl: './task.component.html',
+    styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-  @Input() index: number;
-  @Input() data: TableModel;
+    @Input() index: number;
+    @Input() data: TableModel;
 
-  dragContext: DragContext;
+    dragContext: DragContext;
 
-  constructor() {}
+    constructor() {}
 
-  ngOnInit() {
-      this.dragContext = {};
-  }
+    ngOnInit() {
+        this.dragContext = {};
+    }
 
-  moveStartOfTask(e) {
-    this.dragContext.startPointOfMove = e.screenX;
-      this.dragContext.startDate = this.data.startDate;
-      this.dragContext.duration = this.data.duration;
-    this.dragContext.type = 'start';
-    document.addEventListener('mousemove', this.moveSelectedPointOfTask.bind(this));
-      document.addEventListener('mouseup', () => document.removeEventListener('mousemove', this.moveSelectedPointOfTask.bind(this)));
-  }
+    moveStartOfTask(e) {
+        this.dragContext.startPointOfMove = e.screenX;
+        this.dragContext.startDate = this.data.startDate;
+        this.dragContext.duration = this.data.duration;
+        this.dragContext.type = 'start';
+        document.addEventListener('mousemove', this.moveSelectedPointOfTask);
+        document.addEventListener('mouseup', () =>
+            document.removeEventListener(
+                'mousemove',
+                this.moveSelectedPointOfTask
+            )
+        );
+    }
 
-  moveSelectedPointOfTask(e) {
-      if (this.dragContext.type === 'start') {
-          const delta = e.screenX - this.dragContext.startPointOfMove;
-          this.data.startDate =
-              this.dragContext.startDate + delta;
+    moveSelectedPointOfTask = e => {
+        if (this.dragContext.type === 'start') {
+            const delta = e.screenX - this.dragContext.startPointOfMove;
+            this.data.startDate = this.dragContext.startDate + delta;
 
-          this.data.duration = this.dragContext.duration - (delta * 60000);
-      } else {
-          const delta = this.dragContext.startPointOfMove - e.screenX;
+            this.data.duration = this.dragContext.duration - delta * 60000;
+        } else {
+            const delta = this.dragContext.startPointOfMove - e.screenX;
 
-          this.data.duration = this.dragContext.duration - (delta * 60000);
-      }
-  }
+            this.data.duration = this.dragContext.duration - delta * 60000;
+        }
+    };
 
-  moveEndOfTask(e) {
-      this.dragContext.startPointOfMove = e.screenX;
-      this.dragContext.startDate = this.data.startDate;
-      this.dragContext.duration = this.data.duration;
-      this.dragContext.type = 'end';
-      document.addEventListener('mousemove', event => this.moveSelectedPointOfTask(event));
-      document.addEventListener('mouseup', () => document.removeEventListener('mousemove', this.moveSelectedPointOfTask.bind(this)));
-  }
+    moveEndOfTask(e) {
+        this.dragContext.startPointOfMove = e.screenX;
+        this.dragContext.startDate = this.data.startDate;
+        this.dragContext.duration = this.data.duration;
+        this.dragContext.type = 'end';
+        document.addEventListener('mousemove', this.moveSelectedPointOfTask);
+        document.addEventListener('mouseup', () =>
+            document.removeEventListener(
+                'mousemove',
+                this.moveSelectedPointOfTask
+            )
+        );
+    }
 }
